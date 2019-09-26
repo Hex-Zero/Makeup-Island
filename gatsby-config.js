c2ccb31462f06ea909c59ff17bef1394e8f7de3c
Hex-Zero <hex0@live.com>
2019-09-26 20:38:12 +0100

    looking for state

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,20 +29,5 @@ module.exports = {
         icon: `src/images/fav.png`,
       },
     },
-    {
-      resolve: `gatsby-plugin-react-redux`,
-      options: {
-        // [required] - path to your createStore module
-        pathToCreateStoreModule: "./src/state/createStore",
-        // [optional] - options passed to `serialize-javascript`
-        // info: https://github.com/yahoo/serialize-javascript#options
-        // will be merged with these defaults:
-        serialize: {
-          space: 0,
-          isJSON: true,
-          unsafe: false,
-        },
-      },
-    },
   ],
 }
