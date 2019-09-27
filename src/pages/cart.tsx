747a21112355d44aefbf42b58d52d08a1f365a56
Hex-Zero <hex0@live.com>
2019-09-27 04:26:48 +0100

    good night

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,11 +1,33 @@
 import React from "react"
+import { connect } from "react-redux"
 import Layout from "../components/layout"
+
+const Counter = ({ count, increment }) => (
+  <div>
+    <p>Count: {count}</p>
+    <button onClick={increment}>Increment</button>
+  </div>
+)
+
+const mapStateToProps = ({ count }) => {
+  return { count }
+}
+
+const mapDispatchToProps = dispatch => {
+  return { increment: () => dispatch({ type: `INCREMENT` }) }
+}
+
+const ConnectedCounter = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Counter)
 
 interface Props {}
 
 const cart: React.FC<Props> = ({}) => {
   return (
     <Layout>
+      <ConnectedCounter />
       <div>Here is a cart</div>
     </Layout>
   )
