e511ddf64fac01d5a9e6e1cf5e98323d4344dc10
Hex-Zero <hex0@live.com>
2019-09-27 04:18:48 +0100

    we have state yey

--- gatsby-broser.js
+++
@@ -1,9 +0,0 @@
-const React = require("react")
-const { Provider } = require("react-redux")
-
-const createStore = require("./src/state/createStore")
-const store = createStore()
-
-exports.wrapRootElement = ({ element }) => {
-  return <Provider store={store}>{element}</Provider>
-}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,37 @@
 import React, { useState } from "react"
+import { connect } from "react-redux"
 import ItemCard from "../components/ItemCard"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+const Counter = ({ count, increment }) => (
+  <div>
+    <p>Count: {count}</p>
+    <button onClick={increment}>Increment</button>
+  </div>
+)
+
+const mapStateToProps = ({ count }) => {
+  return { count }
+}
+
+const mapDispatchToProps = dispatch => {
+  return { increment: () => dispatch({ type: `INCREMENT` }) }
+}
+
+const ConnectedCounter = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Counter)
+
 interface Props {}
 
-const IndexPage: React.FC<Props> = () => {
+const IndexPage: React.FC<Props> = ({}) => {
   const [cards, setCards] = useState([1, 2, 3, 4, 5])
   return (
     <Layout>
       <SEO title="Home" />
+      <ConnectedCounter />
       <div className="card-container">
         {cards.map(x => (
           <ItemCard key={x} name={x} />
