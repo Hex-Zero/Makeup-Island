6f869b6c5a5742cf97cb6565a5297da7e77d2b2e
Hex-Zero <hex0@live.com>
2019-10-03 22:45:19 +0100

    hotfix

--- src/context/LocalContext.tsx
+++ src/context/LocalContext.tsx
@@ -1,12 +1,12 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 
 interface Props {}
 
 const LocalContext: React.FC<Props> = ({}) => {
-  const [localProducts, setLocalProducts] = useState(
-    window.localStorage.getItem("DataBase" || null)
-  )
-
+  const [localProducts, setLocalProducts] = useState(null)
+  useEffect(() => {
+    setLocalProducts(window.localStorage.getItem("DataBase" || null))
+  }, [])
   return <div>{localProducts}</div>
 }
 
