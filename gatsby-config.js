2caa8723c65f4d15a3793277e9e352ec44a96842
Hex-Zero <hex0@live.com>
2019-10-21 18:23:55 +0100

    no more typeScript

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,6 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
-    `gatsby-plugin-typescript`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
