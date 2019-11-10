27c0649b8325edabaf47df31f241bca6bd3597c8
Hex-Zero <hex0@live.com>
2019-11-10 18:25:30 +0000

    retreving data from mongo data base

--- src/products/main-sqiueeze.jsx
+++ src/products/main-sqiueeze.jsx
@@ -1,14 +1,37 @@
-import { graphql, useStaticQuery, Link } from "gatsby"
+import { graphql, useStaticQuery } from "gatsby"
 import React, { useEffect, useState } from "react"
-import { TiArrowBackOutline } from "react-icons/ti"
 import Slider from "react-slick"
 import Layout from "../components/layout"
 import Text from "./main-sqiueeze/text"
 
 const MainSqiueeze = () => {
   const [state, setState] = useState([])
+  const [info, setInfo] = useState()
   const data = useStaticQuery(graphql`
-    query {
+    query MyQuery {
+      allMongodbMakeupIslandProducts(
+        filter: {
+          template: { eq: false }
+          local: { eq: "main-squeeze-shadow-palette" }
+        }
+      ) {
+        edges {
+          node {
+            title
+            sku
+            size
+            sale
+            new
+            more
+            inventory
+            description
+            brand
+            ingredients
+            template
+            local
+          }
+        }
+      }
       allFile(filter: { relativeDirectory: { eq: "products/main-sqiueeze" } }) {
         edges {
           node {
@@ -22,16 +45,21 @@ const MainSqiueeze = () => {
       }
     }
   `)
+
   useEffect(() => {
     setState(data.allFile.edges)
   }, [data.allFile.edges, setState])
+  useEffect(() => {
+    setInfo(data.allMongodbMakeupIslandProducts.edges[0].node)
+  }, [data.allMongodbMakeupIslandProducts.edges[0].node, setInfo])
+  console.log(info)
+
   var settings = {
     dots: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
   }
-  console.log(data)
 
   return (
     <Layout>
@@ -50,7 +78,12 @@ const MainSqiueeze = () => {
             })}
           </Slider>
         </div>
-        <Text />
+        <div>
+          <div>{info.title}</div>
+          <div>{info.description}</div>
+          <div>{info.more}</div>
+          <div>{info.sku}</div>
+        </div>
       </div>
     </Layout>
   )
