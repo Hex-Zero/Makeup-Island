0356978a8ffacc52a60f56e2146dc32d2a398d87
Hex-Zero <hex0@live.com>
2019-11-14 19:37:34 +0000

    add seo

--- src/pages/new.jsx
+++ src/pages/new.jsx
@@ -1,8 +1,14 @@
 import React from "react"
 import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 const New = () => {
-  return <Layout>New Products</Layout>
+  return (
+    <Layout>
+      <SEO title="New Product" />
+      New Products
+    </Layout>
+  )
 }
 
 export default New
--- src/pages/sale.jsx
+++ src/pages/sale.jsx
@@ -1,8 +1,14 @@
 import React from "react"
 import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 const Sale = () => {
-  return <Layout>Sale</Layout>
+  return (
+    <Layout>
+      <SEO title="On Sale" />
+      Sale
+    </Layout>
+  )
 }
 
 export default Sale
