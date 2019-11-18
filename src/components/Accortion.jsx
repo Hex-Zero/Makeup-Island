5f807bfb0f9ff9da7946063d81a54c944c7e132a
Hex-Zero <hex0@live.com>
2019-11-18 17:17:00 +0000

    Accordion

---
+++ src/components/Accortion.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Accortion = ({ ingredients }) => {
+  return (
+    <div className="ingredients accordion">
+      {/* {ingredients ? (
+        ingredients.map(item => {
+          return <div key={item}>{item}</div>
+        })
+      ) : (
+        <div></div>
+      )} */}
+    </div>
+  )
+}
+
+export default Accortion
--- src/components/productInfo.jsx
+++ src/components/productInfo.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import AddButton from "./AddButton"
+import Accortion from "./Accortion"
 
 const ProductInfo = ({ title, description, more, ingredients, sku }) => {
   return (
@@ -8,16 +9,7 @@ const ProductInfo = ({ title, description, more, ingredients, sku }) => {
       <div className="description">{description}</div>
       <div className="more">{more}</div>
       <AddButton product={sku} />
-
-      {/* <div className="ingredients">
-        {ingredients ? (
-          ingredients.map(item => {
-            return <div key={item}>{item}</div>
-          })
-        ) : (
-          <div></div>
-        )}
-      </div> */}
+      <Accortion ingredients={ingredients} />
     </div>
   )
 }
