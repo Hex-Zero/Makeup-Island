49bf89af52c913f9d5598fd61a1f0a7b8a9cd92d
Hex-Zero <hex0@live.com>
2019-11-26 14:46:12 +0000

    add contact from

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -8,7 +8,14 @@ const contact = () => {
       <h1>PHONE: ntesrnt</h1>
       <h1>Name: ntesrnt</h1>
       <h1>Info</h1>
-      <h1>Form</h1>
+      <div className="form-container">
+        <form>
+          <h1>E-mail Me</h1>
+          <input placeholder="Your e-mail address"></input>
+          <input placeholder="Subject"></input>
+          <textarea placeholder="Your message"></textarea>
+        </form>
+      </div>
     </Layout>
   )
 }
