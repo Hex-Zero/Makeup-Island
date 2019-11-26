47d9af9da2ed437d2fdbe37c627d56b0141c6264
Hex-Zero <hex0@live.com>
2019-11-26 15:12:07 +0000

    from styled

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -4,13 +4,12 @@ import Layout from "../components/layout"
 const contact = () => {
   return (
     <Layout>
-      <h1>EMAIL: ntesrnt</h1>
+      {/* <h1>EMAIL: ntesrnt</h1>
       <h1>PHONE: ntesrnt</h1>
       <h1>Name: ntesrnt</h1>
-      <h1>Info</h1>
+      <h1>Info</h1> */}
       <div className="form-container">
         <form>
-          <h1>E-mail Me</h1>
           <input placeholder="Your e-mail address"></input>
           <input placeholder="Subject"></input>
           <textarea placeholder="Your message"></textarea>
