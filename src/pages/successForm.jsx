79c353b82af4d69a0329f8a3c7e078f78bba906e
Hex-Zero <hex0@live.com>
2019-11-27 23:54:45 +0000

    successful form submition page

--- src/pages/successForm.jsx
+++ src/pages/successForm.jsx
@@ -1,14 +1,17 @@
 import React from "react"
 import { Link } from "gatsby"
+import Layout from "../components/layout"
 
 const successForm = () => {
   return (
-    <div>
-      Your Message Was Send Thank You.
-      <Link to="/">
-        <button>OK</button>
-      </Link>
-    </div>
+    <Layout>
+      <div className="success-form-submit">
+        <h1>Your Message Was Send Successfully Thank You</h1>
+        <Link to="/" className="ok-button">
+          OK
+        </Link>
+      </div>
+    </Layout>
   )
 }
 
