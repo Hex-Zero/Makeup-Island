eb27be5e51f94638fce14fa443409b2e4a860ce0
Hex-Zero <hex0@live.com>
2019-11-27 17:00:53 +0000

    short from

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import Layout from "../components/layout"
-import { Formik } from "formik"
+// import { Formik } from "formik"
 import SEO from "../components/seo"
 
 const contact = () => {
@@ -16,25 +16,6 @@ const contact = () => {
         <p>
           <label>
             Your Name: <input type="text" name="name" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Email: <input type="email" name="email" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Role:{" "}
-            <select name="role[]" multiple>
-              <option value="leader">Leader</option>
-              <option value="follower">Follower</option>
-            </select>
-          </label>
-        </p>
-        <p>
-          <label>
-            Message: <textarea name="message"></textarea>
           </label>
         </p>
         <p>
