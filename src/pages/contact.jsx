aa88aa3388e13423b91ba63be3bc8a650017dbfa
Hex-Zero <hex0@live.com>
2019-11-27 16:56:33 +0000

    POST

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -12,26 +12,35 @@ const contact = () => {
       <h1>Name: ntesrnt</h1>
       <h1>Info</h1> */}
       <div>
-        <Formik>
-          {() => (
-            <form
-              name="contact"
-              method="post"
-              // netlify-honeypot="bot-field"
-              data-netlify="true"
-            >
-              {/* <input type="hidden" name="bot-field" /> */}
-              <input
-                name="email"
-                type="text"
-                placeholder="Your e-mail address"
-              ></input>
-              {/* <input placeholder="Subject"></input>
-              <textarea placeholder="Your message"></textarea> */}
-              <input type="submit" value="Send" className="send-button"></input>
-            </form>
-          )}
-        </Formik>
+        <form name="contact" method="POST" data-netlify="true">
+          <p>
+            <label>
+              Your Name: <input type="text" name="name" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your Email: <input type="email" name="email" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your Role:{" "}
+              <select name="role[]" multiple>
+                <option value="leader">Leader</option>
+                <option value="follower">Follower</option>
+              </select>
+            </label>
+          </p>
+          <p>
+            <label>
+              Message: <textarea name="message"></textarea>
+            </label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
       </div>
     </Layout>
   )
