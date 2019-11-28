eb6b7757ba6d1005dda2a616649e5ac05891851e
Hex-Zero <hex0@live.com>
2019-11-28 01:20:51 +0000

    accordion open on click

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,8 +1,16 @@
-import React from "react"
+import React, { useState } from "react"
 import { IoIosArrowDown } from "react-icons/io"
 const Accortion = ({ ingredients }) => {
+  const [open, setOpen] = useState("")
+
+  const handleExtendAccordion = () => {
+    !open ? setOpen("extended") : setOpen("")
+  }
   return (
-    <div activeClassName="active" className="ingredients accordion">
+    <div
+      className={`ingredients accordion ${open}`}
+      onClick={() => handleExtendAccordion()}
+    >
       <div className="accortion-title">
         Ingredients <IoIosArrowDown size={30} className="arrow" />
       </div>
