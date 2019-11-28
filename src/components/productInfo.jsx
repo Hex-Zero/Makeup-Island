c3bcb05d7fba122952ff84e9df6804efbb5ba1c8
Hex-Zero <hex0@live.com>
2019-11-28 12:54:24 +0000

    adjust price styling

--- src/components/productInfo.jsx
+++ src/components/productInfo.jsx
@@ -22,7 +22,7 @@ const ProductInfo = ({ title, description, more, ingredients, sku }) => {
 
   return (
     <div className="product-info-container">
-      <div className="price">£{items ? items.price / 100 : false}</div>
+      <div className="info-price">£{items ? items.price / 100 : false}</div>
       <h2 className="title">{title}</h2>
       <h3 className="description">{description}</h3>
       <h4 className="more">{more}</h4>
