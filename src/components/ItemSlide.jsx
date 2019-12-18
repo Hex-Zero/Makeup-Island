2c1dcd476ffa6d52def5fa088fcadffdf068f0ef
Hex-Zero <hex0@live.com>
2019-12-18 20:49:28 +0000

    no need to log

--- src/components/ItemSlide.jsx
+++ src/components/ItemSlide.jsx
@@ -66,7 +66,6 @@ const ItemSlide = ({ condition }) => {
     <div className="more-item-slide">
       <Slider {...settings}>
         {state.map(item => {
-          console.log(info.filter(current => current.node.sku === item.id))
           let status = info.filter(current => current.node.sku === item.id)[0]
             .node
           if (
