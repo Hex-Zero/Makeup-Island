2412ad644296ddc84c0c57f522f87d7789806c81
Hex-Zero <hex0@live.com>
2020-02-02 17:47:42 +0000

    Add products

---
+++ src/products/good-as-gold-shadow-palette.jsx
@@ -0,0 +1,75 @@
+import { graphql, useStaticQuery } from "gatsby"
+import React, { useEffect, useState } from "react"
+import Layout from "../components/layout"
+import ProductInfo from "../components/productInfo"
+import PictureSlides from "../components/pictureSlides"
+import SEO from "../components/seo"
+
+const GoodAsGold = () => {
+  const [state, setState] = useState([])
+  const [info, setInfo] = useState({})
+  const data = useStaticQuery(graphql`
+    query {
+      allMongodbMakeupIslandProducts(
+        filter: { local: { eq: "good-as-gold-shadow-palette" } }
+      ) {
+        edges {
+          node {
+            title
+            sku
+            size
+            sale
+            isnew
+            inventory
+            description
+            brand
+            ingredients
+            local
+          }
+        }
+      }
+      allFile(
+        filter: {
+          relativeDirectory: { eq: "products/good-as-gold-shadow-palette" }
+        }
+      ) {
+        edges {
+          node {
+            childImageSharp {
+              fluid(maxWidth: 1000) {
+                src
+                ...GatsbyImageSharpFluid_tracedSVG
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  useEffect(() => {
+    setState(data.allFile.edges)
+  }, [data.allFile.edges, setState])
+
+  useEffect(() => {
+    let newData = data.allMongodbMakeupIslandProducts.edges[0]
+    setInfo(newData.node)
+  }, [setInfo, data.allMongodbMakeupIslandProducts.edges])
+
+  return (
+    <Layout>
+      <SEO title={info.title} />
+      <div className="Product_Pages">
+        <PictureSlides pictures={state} location="2" />
+        <ProductInfo
+          title={info.title}
+          description={info.description}
+          ingredients={info.ingredients}
+          sku={info.sku}
+        />
+      </div>
+    </Layout>
+  )
+}
+
+export default GoodAsGold
